d9127efb8502616380bb070885d7ff374696748c
Pierre Manchon <pierre.manchon@pm.me>
2020-12-03 14:50:13 +0100

    Create e2.R

---
+++ e2.R
@@ -0,0 +1,18 @@
+library(rgdal)
+library(leaflet)
+library(raster)
+library(mapview)
+
+myspdf2 = readOGR(dsn=getwd(), layer='communes-20190101')
+new_co <- subset(myspdf2, myspdf2$insee %in% c("17180"))
+
+leaflet(new_co) %>%
+  addPolygons(color = "red",
+              weight = 1,
+              smoothFactor = 0.5,
+              opacity = 1.0,
+              fillOpacity = 0.5,
+              fillColor = "red",
+              highlightOptions = highlightOptions(color = "white",
+                                                  weight = 2,
+                                                  bringToFront = TRUE))
